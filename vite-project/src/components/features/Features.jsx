9d867b37c81c0c78682c796391b09a1b12060d1b
emshe49 <zk074909@gmail.com>
2025-02-07 20:27:21 -0800

    form validation add

--- vite-project/src/components/features/Features.jsx
+++ vite-project/src/components/features/Features.jsx
@@ -10,40 +10,40 @@ const Features = () => {
         <div className='flex gap-15 px-8 mt-10'>
             <div className='h-96 w-120 hover:bg-black bg-gray-950 rounded-xl'>
                 <img className='text-red-600 px-10 mt-15' src="/images/icons8-align-left-50.png" alt="" />
-                <h1 className='text-4xl font-bold text-white px-10 mt-3'>Business Strategy</h1>
+                <h1 className='text-3xl font-bold text-white px-10 mt-3'>Business Strategy</h1>
                 <p className='text-xl text-gray-400 mt-7 px-10'>Strategic planning and actions to achieve growth and success.</p>
                 <img className='px-10 mt-10' src="/images/icons8-arrow-48.png" alt="" />
             </div>
             <div className='h-96 w-120 hover:bg-black bg-gray-950 rounded-xl'>
-                <img className='text-red-600 px-10 mt-15' src="/images/icons8-align-left-50.png" alt="" />
-                <h1 className='text-4xl font-bold text-white px-10 mt-3'>Business Strategy</h1>
-                <p className='text-xl text-gray-400 mt-7 px-10'>Strategic planning and actions to achieve growth and success.</p>
+                <img className='text-red-600 h-20 px-10 mt-10' src="/images/icons8-react-100.png" alt="" />
+                <h1 className='text-3xl font-bold text-white px-10 '>React Development</h1>
+                <p className='text-xl text-gray-400 mt-7 px-10'>Creating interactive and fast UIs using the React.js library.</p>
                 <img className='px-10 mt-10' src="/images/icons8-arrow-48.png" alt="" />
             </div>
             <div className='h-96 w-120 hover:bg-black bg-gray-950 rounded-xl'>
-                <img className='text-red-600 px-10 mt-15' src="/images/icons8-align-left-50.png" alt="" />
-                <h1 className='text-4xl font-bold text-white px-10 mt-3'>Business Strategy</h1>
-                <p className='text-xl text-gray-400 mt-7 px-10'>Strategic planning and actions to achieve growth and success.</p>
+                <img className='text-red-600 px-10 mt-10' src="/images/icons8-full-stack-64.png" alt="" />
+                <h1 className='text-3xl font-bold text-white px-10 mt-3'>Full Stack Development (MERN)</h1>
+                <p className='text-xl text-gray-400 mt-5 px-10'>Strategic planning and actions to achieve growth and success.</p>
                 <img className='px-10 mt-10' src="/images/icons8-arrow-48.png" alt="" />
             </div>
         </div>
         <div className='flex gap-15 px-8 mt-10'>
-            <div className='h-92 w-120 hover:bg-black bg-gray-950 rounded-xl'>
-                <img className='text-red-600 px-10 mt-15' src="/images/icons8-align-left-50.png" alt="" />
-                <h1 className='text-4xl font-bold text-white px-10 mt-3'>Business Strategy</h1>
-                <p className='text-xl text-gray-400 mt-7 px-10'>Strategic planning and actions to achieve growth and success.</p>
-                <img className='px-10 mt-10' src="/images/icons8-arrow-48.png" alt="" />
+            <div className='h-96 w-120 hover:bg-black bg-gray-950 rounded-xl'>
+                <img className='text-red-600 px-10 mt-15' src="/images/icons8-database-administrator-48.png" alt="" />
+                <h1 className='text-3xl font-bold text-white px-10 mt-3'>Database Management</h1>
+                <p className=' text-gray-400 mt-7 px-10'>Expertise in managing and optimizing MongoDB for performance.</p>
+                <img className='px-10 mt-7' src="/images/icons8-arrow-48.png" alt="" />
             </div>
             <div className='h-96 w-120 hover:bg-black bg-gray-950 rounded-xl'>
-                <img className='text-red-600 px-10 mt-15' src="/images/icons8-align-left-50.png" alt="" />
-                <h1 className='text-4xl font-bold text-white px-10 mt-3'>Business Strategy</h1>
+                <img className='text-red-600 px-10 mt-15' src="/images/icons8-my-computer-48.png" alt="" />
+                <h1 className='text-3xl font-bold text-white px-10 mt-3'>Computer Science Enthusiast</h1>
                 <p className='text-xl text-gray-400 mt-7 px-10'>Strategic planning and actions to achieve growth and success.</p>
-                <img className='px-10 mt-10' src="/images/icons8-arrow-48.png" alt="" />
+                <img className='px-10 mt-7' src="/images/icons8-arrow-48.png" alt="" />
             </div>
             <div className='h-96 w-120 hover:bg-black bg-gray-950 rounded-xl'>
-                <img className='text-red-600 px-10 mt-15' src="/images/icons8-align-left-50.png" alt="" />
-                <h1 className='text-4xl font-bold text-white px-10 mt-3'>Business Strategy</h1>
-                <p className='text-xl text-gray-400 mt-7 px-10'>Strategic planning and actions to achieve growth and success.</p>
+                <img className='text-red-600 px-10 mt-15' src="/images/icons8-problem-solving-64.png" alt="" />
+                <h1 className='text-3xl font-bold text-white px-10 mt-3'>Problem Solving</h1>
+                <p className='text-xl text-gray-400 mt-7 px-10'>Skilled in analyzing and solving complex problems effectively.</p>
                 <img className='px-10 mt-10' src="/images/icons8-arrow-48.png" alt="" />
             </div>
         </div>
--- vite-project/src/components/project/Project.jsx
+++ vite-project/src/components/project/Project.jsx
@@ -5,8 +5,8 @@ const Project = () => {
     <>
     
     <div className='h-full w-full flex flex-col  bg-gray-900 '>
-        <h3 className='text-red-600 text-center mt-20 text-3xl'>Check out my portfolio and share your feedback!</h3>
-        <h1 className='text-7xl text-white text-center py-4 font-bold'>My Projects</h1>
+        <h3 className='text-red-500 text-center mt-20 text-xl'>Check out my portfolio and share your feedback!</h3>
+        <h1 className='text-6xl text-white text-center py-4 font-bold'>My Projects</h1>
         <div className='flex h-full w-full p-14 gap-16'>
             <div className='h-[650px] w-120 hover:bg-black bg-gray-950 rounded-xl'>
                 <img className='text-red-600 px-10 mt-20 rounded-b-xl hover:rounded-b-lg' src="/images/1.jpg" alt="" />
@@ -27,17 +27,17 @@ const Project = () => {
         </div>
         <div className='flex h-full w-full p-14 gap-16'>
             <div className='h-[650px] w-130 hover:bg-black bg-gray-950 rounded-xl'>
-                <img className='text-red-600 px-10 mt-20 rounded-b-xl hover:rounded-b-lg' src="/images/1.jpg" alt="" />
+                <img className='text-red-600 px-10 mt-20 rounded-b-xl hover:rounded-b-lg' src="/images/ES1.jpg" alt="" />
                 <h3 className='text-red-600 mt-6 p-10 text-xl font-semibold'>DOC APPOINTEMENT WEB APP</h3>
                 <p className='px-10 text-white text-xl'>A fully functional doctor appointment booking app allowing users to schedule appointments, manage profiles, and get real-time notifications.</p>
             </div>
             <div className='h-[650px] w-120 hover:bg-black bg-gray-950 rounded-xl'>
-                <img className='text-red-600 px-10 mt-20 rounded-b-md' src="/images/2.jpg" alt="" />
+                <img className='text-red-600 hover:shadow-2xl   hover:p-5 px-10 mt-20 rounded-3xl' src="images/ES3.jpg" alt="" />
                 <h3 className='text-red-600 mt-6 p-10 text-xl font-semibold'>REAL ESTATE WEB APP</h3>
                 <p className='px-10 text-white text-xl'>A responsive real estate web application that showcases properties for buying, renting, or selling, complete with filtering and detailed views.</p>
             </div>
             <div className='h-[650px] w-120 hover:bg-black hover:shadow-gray-900 bg-gray-950 rounded-xl'>
-                <img className='text-red-600 hover:shadow-2xl hover:p-5 px-10 mt-20 rounded-b-md' src="/images/3.jpg" alt="" />
+                <img className='text-red-600 hover:shadow-2xl hover:p-5 px-10 mt-20 rounded-b-md' src="/images/ES3.jpg" alt="" />
                 <h3 className='text-red-600 mt-6 p-10 text-xl font-semibold'>ECOMMERCE WEBSITE</h3>
                 <p className='px-10 text-white text-xl'>A scalable e-commerce platform supporting product listing, cart functionality, and payment integration</p>
             </div>
