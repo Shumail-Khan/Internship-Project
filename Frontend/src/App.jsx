d168bc84d5c9222bd7f8e1813ecd0bdc6d44aeb5
emshe49 <zk074909@gmail.com>
2025-02-08 15:37:24 -0800

    Routes setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Docter from './pages/Docter'; 
+import Login from './pages/Login' 
+import About from './pages/About';
+import Contact from './pages/Contact';
+import MyProfile from './pages/MyProfile';
+import MyAppointment from './pages/MyAppointment';
+import Appointment from './pages/Appointment';
 
 const App = () => {
   return (
-    <div className='bg-green-600'>App</div>
-  )
-}
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/docter" element={<Docter/>} />
+        <Route path="/docter/:speciality" element={<Docter/>} />
+        <Route path="/login" element = {<Login/>} />
+        <Route path="/about" element = {<About/>} />
+        <Route path="/contact" element = {<Contact/>}/>
+        <Route path='/myProfile' element= {<MyProfile/>}/>
+        <Route path='/myAppointment' eleemnt = {<MyAppointment/>}/>
+        <Route path="Appointment/:dicId" element={<Appointment/>} />
 
-export default App
+
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
---
+++ Frontend/src/pages/Docter.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Docter = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Docter
