8f090794c39c1a2d815831f45681b0a81d360e5d
emshe49 <zk074909@gmail.com>
2025-02-09 20:58:22 -0800

    useNivigateHook and useState

--- Frontend/src/components/Nav.jsx
+++ Frontend/src/components/Nav.jsx
@@ -1,37 +1,59 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 
 const Nav = () => {
+    const navigate =useNavigate()
+    const [token,setToken] = useState(false)
   return (
     <div className='flex justify-between items-center py-4 mb-5 border-b border-b-gray-400'>
-        <img className='w-44 cursor-pointer' src='/Images/logo.svg' alt="" />
-        <ul className='hidden md:flex items-start gap-5 font-medium '>
-            <NavLink  to="/">
-                <li className='py-1'>HOME</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w/35 m-auto' />
-            </NavLink>
-            
-            <NavLink to="/docter">
-                <li className='py-1'>All DOCTER</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w/35 m-auto' />            
-            </NavLink>
-            <NavLink  to="/about">
-                <li className='py-1'>ABOUT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w/35 m-auto' />
-            </NavLink>
-            <NavLink  to="/contact">
-                <li className='py-1'>CONTACT</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w/35 m-auto' />
-            </NavLink>
-        </ul>
-        <div>
-            <button className='bg-blue-600 rounded-4xl p-2'>Create Account</button>
-        </div>
+      <img className='w-44 cursor-pointer' src='/Images/logo.svg' alt="" />
+      <ul className='hidden md:flex items-start gap-5 font-medium'>
+        <NavLink to="/" exact>
+          {({ isActive }) => (
+            <li className={`py-1 ${isActive ? 'border-b border-b-blue-600 ' : ''}`}>
+              HOME
+            </li>
+          )}
+        </NavLink>
+
+        <NavLink to="/docter">
+          {({ isActive }) => (
+            <li className={`py-1 ${isActive ? 'border-b border-b-blue-600' : ''}`}>
+              All DOCTER
+            </li>
+          )}
+        </NavLink>
+
+        <NavLink to="/about">
+          {({ isActive }) => (
+            <li className={`py-1 ${isActive ? ' border-b border-b-blue-600' : ''}`}>
+              ABOUT
+            </li>
+          )}
+        </NavLink>
+
+        <NavLink to="/contact">
+          {({ isActive }) => (
+            <li className={`py-1 ${isActive ? 'border-b border-b-blue-600' : ''}`}>
+              CONTACT
+            </li>
+          )}
+        </NavLink>
+      </ul>
+      <div className='flex item-center gap-4'>
+        {
+            token
+            ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+                <img className='rounded-full w-8' src="/Images/profile_pic.png" alt="" />
+                <img className='w-2.5' src="/Images/dropdown_icon.svg" alt="" />
+            </div>
+            :<button onClick={()=> navigate('/Login')} className='bg-blue-600 rounded-full px-8 py-3 font-light hidden md:block text-white'>Create Account</button>  
+        }
+        
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Nav
+export default Nav;
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
+    <div className='items-center bg-gray-500 w-[400px] h-[300px] sm:mx-[30%] mt-40'>
+      <h1 className='mt-24 ml-23'>email</h1>
+      <input className='mt-24 ml-23  bg-black text-white border-2 border-white rounded-xl' type="text" placeholder='enter your name' name="" id="" />
         
     </div>
   )
